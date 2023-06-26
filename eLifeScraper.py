283c784fbaf0e5106b5bde716ed9411424c1fdeb
vickyhwang77 <67029487+vickyhwang77@users.noreply.github.com>
2023-06-26 09:24:44 -0400

    Add files via upload

--- eLifeScraper.py
+++ eLifeScraper.py
@@ -5,67 +5,181 @@
 
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import time
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
 
-# structure of the website: list of reviewed papers listed on 22 pages, click on 
-# any paper link, and the actual review is under a link within the paper
+def extract_assessment(topic, num_pages, rating):
+    # Step 1: Setup 
 
+    # url = the webpage that lists all the papers. 
+    url = "https://elifesciences.org/reviewed-preprints"
 
-# Step 1: Setup 
+    # For all the papers, find the link to the paper.
+    base_url = "https://elifesciences.org"
+    base_url2 = "https://elifesciences.org/reviewed-preprints"
+    topics = []
+    titles = []
+    assessments = []
+    scores = []
 
-# url = the webpage that lists all the papers. 
-url = "https://elifesciences.org/reviewed-preprints"
-result = requests.get(url)
-# create BeautifulSoup object to parse html content of webpage
-soup = BeautifulSoup(result.content, "html.parser")
+    for page in range(1, num_pages+1):
 
-# Step 2. Get links to all the papers listed.
+        result = requests.get(url)
+        # create BeautifulSoup object to parse html content of webpage
+        soup = BeautifulSoup(result.content, "html.parser")
 
-# Get the list containing all the papers.
-papers_list = soup.find("ol", class_="listing-list")
-# Get all the papers in the list.
-papers = papers_list.find_all("li", class_="listing-list__item")
+        # Step 2. Get links to all the papers listed.
 
-# It is possible to do multiple pages, or all at this point. 
-# Finding all papers is an (nearly) endless loop.  
-
-# For all the papers, find the link to the paper.
-base_url = "https://elifesciences.org"
-
-for paper in papers:
-
-    paper_link = paper.find("a", class_ ="teaser__header_text_link")
-    paper_link_url = base_url + paper_link["href"]
-
-    paper_content = requests.get(paper_link_url)
-    paper_soup = BeautifulSoup(paper_content.content, "html.parser")
-    # Find all paragraphs, keep the first one
-    eLife_section = paper_soup.find("div", class_="review-content_body")
-    eLife_paragraphs = eLife_section.find_all("p")
-    eLife_assessment = eLife_paragraphs[1]
-    print(eLife_assessment.text.strip())
-    print("\n")
-   
-
-
-
-
-# 3. Iterate over each paper link and extract the Peer review paragraph
-"""for paper_link in paper_links:
+        # Get the list containing all the papers.
+        papers_list = soup.find("ol", class_="listing-list")
+        # Get all the papers in the list.
+        papers = papers_list.find_all("li", class_="listing-list__item")
     
-    # Get the linked page for curr paper
-    curr_paper = requests.get(paper_link['href'])
+        # For each paper, get it's link:
+        for paper in papers:
+
+            paper_link = paper.find("a", class_ ="teaser__header_text_link")
+            paper_link_url = base_url + paper_link["href"]
+
+            try: 
+            # Using the paper link, access paper contents:
+                paper_content = requests.get(paper_link_url)
+                paper_soup = BeautifulSoup(paper_content.content, "html.parser")
+
+                # Find the paper topic
+                paper_topic = paper_soup.find("a", class_="article-flag__link")
+                
+                # Find the paper title
+                paper_title = paper_soup.find("h1", class_="title")
+                print(paper_title.text)
+
+                # Find all paragraphs, keep the first one
+                eLife_section = paper_soup.find("div", class_="review-content_body")
+                
+                eLife_paragraphs = eLife_section.find_all("p")
+                eLife_assessment = eLife_paragraphs[1]
+                paragraph = eLife_assessment.text.strip()  
+            
+
+                # Keep only the papers whose scores that match the given score
+                paper_score = simpleAnalysis(paragraph)
+
+                if rating == "Poor" and paper_score < 0:
+                    if topic == "Any" or topic == paper_topic.text.strip():
+                        topics.append(paper_topic.text.strip())
+                        titles.append(paper_title.text.strip())
+                        assessments.append(paragraph)
+                        scores.append(paper_score)
+                    
+                elif rating == "Mediocre" and 0 <= paper_score < 1:
+                    if topic == "Any" or topic == paper_topic.text.strip():
+                        topics.append(paper_topic.text.strip())
+                        titles.append(paper_title.text.strip())
+                        assessments.append(paragraph)
+                        scores.append(paper_score)
+                    
+                elif rating == "Good" and paper_score == 1:
+                    if topic == "Any" or topic == paper_topic.text.strip():
+                        topics.append(paper_topic.text.strip())
+                        titles.append(paper_title.text.strip())
+                        assessments.append(paragraph)
+                        scores.append(paper_score)
+                    
+                else:
+                    continue
     
-    paper_soup = BeautifulSoup(curr_paper.content, "html.parser")
+                time.sleep(2)
 
-    # Locate the Peer review tab on linked page
-    tab = paper_soup.find("a", text="Peer review")
+            except AttributeError:
+                # Handle the case when the attribute of the webpage is not found
+                continue
 
-    if tab:
-        peer_review_paragraph = tab.find_next("p").text
-        print(peer_review_paragraph)
-    else:
-        print("Paragraph not found.")"""
+        # Go to next page of papers
+            new_url = base_url2 + "?page=" + str(page+1)     # page numbering begins on page 2
+            url = new_url
 
+    # Create Database
+    df = pd.DataFrame({"Topic": topics, "Title": titles, "Assessment": assessments, "Score": scores}) 
+    print(df)
+ 
+    df.to_csv("eLife.csv", index=False)  # Save Data to CSV file
+
+def simpleAnalysis(paragraph):
+
+    best_words = ["landmark, exceptional"]      # 1
+    great_words = [ "fundamental", "compelling"] # 0.75
+    good_words = ["important"]                  # 0.5
+    pos_words = ["valuable", "convincing"]      # 0.25
+    neu_words = ["useful", "solid"]             # 0
+    neg_words = ["partially supported", "further strengthened", "require additional"] # -0.25
+    bad_words = ["inadequate"]                # - 0.75
+    worst_words = ["incomplete"]                # - 1
+
+    num_best = 0
+    num_great = 0
+    num_good = 0 
+    num_pos = 0
+    num_neu = 0
+    num_neg = 0
+    num_bad = 0
+    num_worst = 0
+    
+    stop_words = set(stopwords.words('english'))        # remove unecessary words
+    num_words = 0      # initialize new counter for paragraph after stop words are filtered
+    paragraph = paragraph.lower()
+
+    # Generate n-grams from tokenized words
+    tokens = word_tokenize(paragraph)
+    n = 2  # Change this to the desired n-gram size
+    ngrams_list = list(ngrams(tokens, n))
+
+
+    # Check words:
+    for word in tokens:
+        if word not in stop_words:
+            num_words += 1
+            if word in best_words:
+                num_best += 1
+            elif word in great_words:
+                num_great += 1
+            elif word in good_words:
+                num_good += 1
+            elif word in pos_words:
+                num_pos += 1
+            elif word in neu_words:
+                num_neu += 1
+            elif word in neg_words:
+                num_neg += 1
+            elif word in bad_words:
+                num_bad += 1
+            elif word in worst_words:
+                num_worst += 1
+
+    # Check phraes:
+    for ngram in ngrams_list:
+        phrase = " ".join(ngram)
+        if phrase in neg_words:
+            num_neg += 1
+
+     
+    meaningful_words = num_best+num_great+num_good+num_pos+num_neu+num_neg+num_bad+num_worst
+    score = ((num_best)+(0.75*num_great)+(0.5*num_good)+(0.25*num_pos)+(0*num_neu)+(-0.25*num_neg)+(-0.75*num_bad)+(-1*num_worst)) / meaningful_words
+    print(score)
+
+    return score
+
+    
+def main():
+    #format: extract_assessment("Topic, number pages to search, paper rating")
+
+    extract_assessment("Any", 2, "Good")
+
+if __name__ == "__main__":
+    main()
 
 
 
---
+++ f1000Scraper.py
@@ -0,0 +1,120 @@
+""" 06/08/23 by Victoria Hwang
+    This script uses webscraping to extract peer review from F1000's reviewed articles,
+    with the goal to calculate sentiment scores from these texts and use them to predict 
+    the peer review result
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import time
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
+
+
+def extract_assessment():
+    # Step 1: Setup 
+
+    # url = the webpage that lists all the papers. 
+    url = "https://f1000research.com/browse/articles"
+    scores = []             # -1 = not approved
+                            # 0 = approved w/ reservations
+                            # 1 = approved
+  
+    titles = []
+    review_columns = []
+    review_texts = []
+    data = []
+    #for page in range(1, num_pages+1):
+
+    # Step 2. Get links to all the papers listed.
+    result = requests.get(url)
+    # create BeautifulSoup object to parse html content of webpage
+    soup = BeautifulSoup(result.content, "html.parser")
+
+    # Get the list containing all the papers.
+    papers_list = soup.find("div", class_="article-listing f1000research")
+    papers = papers_list.find_all("div", class_= "article-browse-wrapper f1r-searchable")
+  
+
+    # Extract the reviews from reviewed papers:
+    for paper in papers: 
+        # Each paper may have more than one review:
+   
+        # and also an accompanying decision: Yes, Maybe, No:  
+        num_reviews = 0 
+        num_approved = 0
+        num_maybe = 0
+        num_disproved = 0
+
+        awaiting_review = paper.find("span", class_="peer-review-status")
+
+        # skip papers without reviews
+        if awaiting_review: 
+            continue 
+
+        else: 
+            paper_link = paper.find("a", class_ ="article-link")
+            paper_link_url = paper_link["href"]
+
+            # Using the paper link, access paper contents:
+            paper_content = requests.get(paper_link_url)
+            paper_soup = BeautifulSoup(paper_content.content, "html.parser")
+            paper_title = paper_soup.find("h1", class_="js-article-title")
+            titles.append(paper_title.text)
+          
+            
+            # Inside the paper, there are multiple peer reviews. For each of them,
+            # extract the score and review:
+            reviews = paper_soup.find_all("div", \
+            class_ ="p-article__sidebar-view u-pl u-hide js-article-sidebar-view")
+        
+            for review in reviews:
+
+                num_reviews += 1        # for calculating overall report score
+
+                # Get reviewer decision:
+                decision = review.find("span", class_= \
+                "p-article__report-status t-caption u-upper-spacing u-upper u-ib u-middle u-weight--md")
+                
+
+                # Calculate score based on reviewer decision
+                if decision.text == "Approved":
+                    num_approved += 1
+                elif decision.text == "Approved With Reservations":
+                    num_maybe += 1
+                else: 
+                    num_disproved +=1
+
+                #print("Decision: " + decision.text + "\n")
+
+
+                paragraph = review.find("div", class_= "")
+                review_texts.append(paragraph.text)
+                
+                # To handle possible multiple reviews
+                review_columns.append(f"Review {num_reviews}")
+
+            
+            reviewer_score = (num_approved + (0*num_maybe) + (-1*num_disproved)) / num_reviews
+            scores.append(reviewer_score)
+
+            data.append({
+                "Title": paper_title.text.strip(),
+                "Reviewer Score": reviewer_score,
+                **dict(zip(review_columns, review_texts))
+            })
+
+    df = pd.DataFrame(data)
+    
+    df.to_csv("/Users/vickyhwang/delt4/quantify knowledge/scrapers/f1000.csv", index=False)
+
+
+
+def main():
+    extract_assessment()
+
+if __name__ == "__main__":
+    main()
